648693cb7123dd428da010e2aea881c350077ed8
COR Students <corstudents@Matts-MBP-97.localdomain>
2018-11-02 11:50:57 -0400

    Finished

--- scripts.js
+++ scripts.js
@@ -1,3 +1,18 @@
-$(document).ready(function() {
+function myCalculator(id) {
+    document.calc.display.value +=id
     
-});
+}
+function clearScr() {
+    document.calc.display.value=""
+}
+function modNum() {
+     
+    document.calc.display.value +="%"
+
+}
+function calculate() {
+    
+        input = eval(document.calc.display.value)
+        document.calc.display.value = input
+      
+}
